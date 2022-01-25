25004ad1cdd18953170db8c4c0932f04f38368ff
tuananhsavvycom <97879754+tuananhsavvycom@users.noreply.github.com>
2022-01-25 19:00:55 +0700

    Update ex01.go

--- ex01.go
+++ ex01.go
@@ -5,26 +5,19 @@ import (
 	"time"
 )
 
-// func crawl data
+
 func crawlData(data int) {
 	fmt.Println("Data crawled: ", data)
 }
 
-// func main
+
 func main() {
-	fmt.Print("Crawling data..\n")
-
-	// Create number of URLs
+	fmt.Print("Crawling data..\n")	
 	urlCrawl := 1000
-	// Make buffered channel
 	buffChan := make(chan int, urlCrawl)
-
-	// Push data in channel
 	for i := 1; i <= urlCrawl; i++ {
 		buffChan <- i
 	}
-
-	//Make go func in a loop to print data
 	for i := 1; i <= 5; i++ {
 		go func() {
 			for data := range buffChan {
